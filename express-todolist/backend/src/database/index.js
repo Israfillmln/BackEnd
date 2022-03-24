25471ffbbb9b2ff5618ee32f15204a28b649ec5b
Israfillmln <israfilmaulana925@gmail.com>
2022-03-24 21:12:26 +0700

    tugas todolist

---
+++ express-todolist/backend/src/database/index.js
@@ -0,0 +1,21 @@
+const todos = [
+    {
+      action: "Balap Mobil",
+      isDone: true,
+      id: 1
+    },
+    {
+        action: "Balap Motor",
+        isDone: true,
+        id: 2
+    },
+    {
+        action: "Balap Lari",
+        isDone: false,
+        id: 3
+    }
+]
+
+module.exports = {
+    todos
+}
